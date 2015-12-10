92b36dbf032ca014f1d9c459a783ae9d7f1ccead
Arvind Iyer <arvindi.me@gmail.com>
2015-12-10 23:55:21 +0800

    Added Firebase! :fire:

--- main.js
+++ main.js
@@ -1,42 +1,47 @@
 (function(document) {
   'use strict';
-  var app = document.querySelector("#app");
+
+  var app = document.querySelector('#app');
   app.firebaseURL = 'https://polycodelabtodo.firebaseio.com';
   app.firebaseProvider = 'anonymous';
-  app.items = [
-    {
-      done: true,
-      text: 'Write a TODO app'
-    },
-    {
-      done: false,
-      text: 'Use Firebase'
-    }
-  ];
-  
+
+  app.items = [];
+
+  app.updateItems = function(snapshot) {
+    this.items = [];
+    snapshot.forEach(function(childSnapshot) {
+      var item = childSnapshot.val();
+      item.uid = childSnapshot.key();
+      this.push('items', item);
+    }.bind(this));
+  };
+
   app.addItem = function(event) {
-    event.preventDefault();
-    this.push('items', {
+    event.preventDefault(); // Don't send the form!
+    this.ref.push({
       done: false,
       text: app.newItemValue
     });
-    this.newItemValue = '';
+    app.newItemValue = '';
   };
-  
+
   app.toggleItem = function(event) {
-    event.model.set('done', !event.model.item.done);
+    this.ref.child(event.model.item.uid).update({done: event.model.item.done});
   };
+
   app.deleteItem = function(event) {
-    this.splice('items', event.model.index, 1);
+    this.ref.child(event.model.item.uid).remove();
   };
-  
-  //Firebase event handling
-  app.onFirebaseError = function(event) {
-    this.$.errorToast.text = event.detail.message;
+
+  app.onFirebaseError = function(e) {
+    this.$.errorToast.text = e.detail.message;
     this.$.errorToast.show();
   };
-  
-  app.onFirebaseLogin = function(event) {
-    this.ref = new Firebase(this.firebaseURL + '/user/' + event.detail.user.uid);
+  app.onFirebaseLogin = function(e) {
+    this.ref = new Firebase(this.firebaseURL + '/user/' + e.detail.user.uid);
+    this.ref.on('value', function(snapshot) {
+      app.updateItems(snapshot);
+    });
   };
+
 })(document);
