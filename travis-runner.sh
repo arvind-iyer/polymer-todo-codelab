cd27c0076a6ecc90044c94064ccc3bc28aecbab0
Arvind Iyer <arvind-iyer@users.noreply.github.com>
2015-12-11 01:23:14 +0800

    Update travis-runner.sh

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -o pipefail
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
+if [ "$TRAVIS_BRANCH" = "deploy" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  git config --global user.email "samccone@gmail.com" && \
+  git config --global user.email "arvindiiyer.me@gmail.com" && \
   git config --global user.name "auto deployer" && \
   echo "Deploying!" && \
   sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
